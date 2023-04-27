35d182d7ebf3ddb569e7a7bc9869bcf47457fcc9
Natalia Szczepanowska <szczepanowska.n@gmail.com>
2023-04-27 22:25:29 +0200

    new solution

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import logo from '/Users/szczepanowska/code/Szczepanowska/react/ravenous/src/logo.svg';
-import './App.css';
-import BusinessList from '../BusinessList/BusinessList';
-import SearchBar from '../SearchBar/SearchBar';
+import React from "react";
+import styles from "./App.module.css";
 
-class App extends Component {
-  render() {
-return (
-  <div className="App">
-    <h1>ravenous</h1>
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <h1>ravenous</h1>
       <SearchBar />
       <BusinessList />
-  </div>
-)
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- ravenous/src/components/BusinessList/BusinessList.js
+++ ravenous/src/components/BusinessList/BusinessList.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import './BusinessList.css';
-import Business from '../Business/Business';
+import React from "react";
+import styles from "./BusinessList.module.css";
 
-class BusinessList extends React.Component {
-  render() {
-    return (
-      <div className="BusinessList">
-          <Business />
-          <Business />
-          <Business />
-          <Business />
-          <Business />
-          <Business />
-</div>
-    )
-  }
-}
+import Business from "../Business/Business";
+
+const BusinessList = () => {
+  return (
+    <div className={styles.BusinessList}>
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+    </div>
+  );
+};
 
 export default BusinessList;
